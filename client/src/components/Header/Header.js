570c8346ce6a28c298906a1a2dc0f65f8b9ae867
Kyle Friel <businessfriel@gmail.com>
2021-07-18 17:05:43 -0400

    Google fonts imported via CSS @import. Header component and styles done.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className={`primary ${styles["header-container"]}`}>
             <div className={`container ${styles["header"]}`}>
-                <h1 className="text-primary">SortApp</h1>
+                <h1 className="text-primary">SortApp.</h1>
             </div>
         </div>
     );
