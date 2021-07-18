427d0d79a75a27381c08a876485eb2dd937be6f9
Kyle Friel <businessfriel@gmail.com>
2021-07-18 17:00:17 -0400

    MeyerWeb css reset added.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,9 @@ import styles from "./Header.module.css";
 const Header = () => {
     return (
         <div className={`primary ${styles["header-container"]}`}>
-            <div className={`container ${styles["header"]}`}></div>
+            <div className={`container ${styles["header"]}`}>
+                <h1 className="text-primary">SortApp</h1>
+            </div>
         </div>
     );
 };
