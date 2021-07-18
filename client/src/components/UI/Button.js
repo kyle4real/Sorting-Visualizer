1ef27bbee6c8518f4135cbfc0344f6fc6a128daf
Kyle Friel <businessfriel@gmail.com>
2021-07-18 17:32:23 -0400

    Added styled-components. Created Button component as styled component so that, later, we can easily disable them dynamically.

---
+++ client/src/components/UI/Button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    padding: 0.5rem 1.5rem;
+    font: inherit;
+    background: #1dc690;
+    border: 1px solid #1dc690;
+    box-shadow: 0 2px 2px -2px rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+`;
+
+export default Button;
