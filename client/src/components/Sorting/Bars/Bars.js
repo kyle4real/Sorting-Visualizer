8b4e14b18c3c835ca9d96f3c7de9a51a0f93a3f3
Kyle Friel <businessfriel@gmail.com>
2021-07-18 20:28:11 -0400

    Bars component done.

---
+++ client/src/components/Sorting/Bars/Bars.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Bars.module.scss";
+
+const Bars = () => {
+    return <div className={styles["bars-container"]}></div>;
+};
+
+export default Bars;
--- client/src/components/Sorting/Sorting.js
+++ client/src/components/Sorting/Sorting.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Bars from "./Bars/Bars";
 import styles from "./Sorting.module.scss";
 
 const Sorting = () => {
     return (
         <div className={styles["sorting-container"]}>
-            <div className={`container ${styles["sorting"]}`}></div>
+            <div className={`container ${styles["sorting"]}`}>
+                <Bars />
+            </div>
         </div>
     );
 };
