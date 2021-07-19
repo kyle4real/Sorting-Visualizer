a7d0434d928387b9e6368fc93511134aa686eebb
Kyle Friel <businessfriel@gmail.com>
2021-07-19 01:56:15 -0400

    ProgressBar component, progress but still not working correctly.

--- client/src/components/Sorting/SetupProgress/ProgressBar/ProgressBar.js
+++ client/src/components/Sorting/SetupProgress/ProgressBar/ProgressBar.js
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./ProgressBar.module.scss";
 
 const ProgressBar = () => {
-    return <div className={styles["progress-bar"]}></div>;
+    const [on, setOn] = useState(false);
+    const [direction, setDirection] = useState(false);
+    const [width, setWidth] = useState(0);
+    const intervalRef = useRef();
+
+    const handleClick = () => {
+        setOn((p) => !p);
+        setDirection((p) => !p);
+    };
+
+    useEffect(() => {
+        // prevents start on render
+        if (!on) return;
+        setOn((p) => !p);
+        // prevents clearing an undefined interval
+        if (intervalRef.current !== undefined) clearInterval();
+        // start interval
+        var i = width;
+        intervalRef.current = setInterval(() => {
+            if (direction) {
+                if (i >= 100) {
+                    setOn((p) => !p);
+                    setDirection((p) => !p);
+                    clearInterval(intervalRef.current);
+                    return;
+                } else {
+                    i++;
+                    setWidth((p) => (p += 1));
+                }
+            } else {
+                if (i <= 0) {
+                    setOn((p) => !p);
+                    setDirection((p) => !p);
+                    clearInterval(intervalRef.current);
+                    return;
+                } else {
+                    i--;
+                    setWidth((p) => (p -= 1));
+                }
+            }
+        }, 10);
+    }, [on]);
+
+    return (
+        <>
+            <div className={styles["progress-bar"]}>
+                <div className={styles["progress-bar__fill"]} style={{ width: `${width}%` }}></div>
+                <button onClick={handleClick}>c</button>
+            </div>
+        </>
+    );
 };
 
 export default ProgressBar;
--- client/src/components/Sorting/SetupProgress/SetupProgress.js
+++ client/src/components/Sorting/SetupProgress/SetupProgress.js
@@ -6,6 +6,11 @@ import styles from "./SetupProgress.module.scss";
 const SetupProgress = () => {
     return (
         <div className={styles["setupProgress-container"]}>
+            <div className={styles["progress-titles"]}>
+                <p>1) choose sort.</p>
+                <p>(not ready.)</p>
+                <p>2) choose data.</p>
+            </div>
             <div className={styles["setupProgress"]}>
                 <CompletedNode />
                 <ProgressBar />
