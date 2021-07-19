f9cdee1294c11cb1517e0b51b1a537001f9b3a4d
Kyle Friel <businessfriel@gmail.com>
2021-07-18 21:07:53 -0400

    Bar component.

---
+++ client/src/components/Sorting/Bars/Bar/Bar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Bar.module.scss";
+
+const Bar = () => {
+    return <div className={styles.bar}></div>;
+};
+
+export default Bar;
--- client/src/components/Sorting/Bars/Bars.js
+++ client/src/components/Sorting/Bars/Bars.js
@@ -1,8 +1,31 @@
 import React from "react";
+import Bar from "./Bar/Bar";
 import styles from "./Bars.module.scss";
 
+const data = [
+    { id: 1, height: 50 },
+    { id: 1, height: 24 },
+    { id: 1, height: 87 },
+    { id: 1, height: 92 },
+    { id: 1, height: 37 },
+    { id: 1, height: 3 },
+    { id: 1, height: 13 },
+    { id: 1, height: 69 },
+];
+
 const Bars = () => {
-    return <div className={styles["bars-container"]}></div>;
+    return (
+        <div className={styles["bars-container"]}>
+            <div
+                className={styles["bars"]}
+                style={{ gridTemplateColumns: `repeat(${data.length}, 1fr)` }}
+            >
+                {data.map((dataPoint) => (
+                    <Bar key={dataPoint.id} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Bars;
