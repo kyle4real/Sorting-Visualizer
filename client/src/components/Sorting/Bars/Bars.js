e8d00cbcf3287769feed679642d5ee805b371726
Kyle Friel <businessfriel@gmail.com>
2021-07-19 20:57:42 -0400

    Bars increased makeshift data. Bar component styling update - removed borders, made Bars display flex (instead of grid), and changed color.

--- client/src/components/Sorting/Bars/Bars.js
+++ client/src/components/Sorting/Bars/Bars.js
@@ -2,16 +2,27 @@ import React from "react";
 import Bar from "./Bar/Bar";
 import styles from "./Bars.module.scss";
 
-const data = [
-    { id: 1, height: 50 },
-    { id: 2, height: 24 },
-    { id: 3, height: 87 },
-    { id: 4, height: 92 },
-    { id: 5, height: 37 },
-    { id: 6, height: 3 },
-    { id: 7, height: 13 },
-    { id: 8, height: 69 },
-];
+// const data = [
+//     { id: 1, height: 50 },
+//     { id: 2, height: 24 },
+//     { id: 3, height: 87 },
+//     { id: 4, height: 92 },
+//     { id: 5, height: 37 },
+//     { id: 6, height: 3 },
+//     { id: 7, height: 13 },
+//     { id: 8, height: 69 },
+// ];
+
+const limit = 100;
+const num = 200;
+const data = [];
+for (let i = 0; i < num; i++) {
+    const newObj = {
+        id: Math.round(Math.random() * 1000),
+        height: Math.round(Math.random() * limit),
+    };
+    data.push(newObj);
+}
 
 const Bars = () => {
     return (
