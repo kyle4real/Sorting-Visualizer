7d05d8afea1110199050f41062e826e450f33e47
Kyle Friel <businessfriel@gmail.com>
2021-07-19 21:36:53 -0400

    Adjusting sorting component styles and its child components styles - heights and display.

---
+++ client/src/components/Sorting/Controls/Controls.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Controls.module.scss";
+
+const Controls = () => {
+    return (
+        <div className={styles["controls-container"]}>
+            <div className={styles["controls"]}>controls</div>
+        </div>
+    );
+};
+
+export default Controls;
--- client/src/components/Sorting/Sorting.js
+++ client/src/components/Sorting/Sorting.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Bars from "./Bars/Bars";
+import Controls from "./Controls/Controls";
 import SetupProgress from "./SetupProgress/SetupProgress";
 import styles from "./Sorting.module.scss";
 
-const Sorting = ({ sortSelection, dataSelection }) => {
+const Sorting = ({ sortSelection, dataSelection, dataAmount }) => {
     return (
         <div className={styles["sorting-container"]}>
             <div className={`container ${styles["sorting"]}`}>
                 <SetupProgress sortSelection={sortSelection} dataSelection={dataSelection} />
                 <Bars />
+                <Controls />
             </div>
         </div>
     );
