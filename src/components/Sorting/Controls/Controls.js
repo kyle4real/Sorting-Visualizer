6e9fde56291a3df4cd81f3113d2d4ed3bb54fd9c
Kyle Friel <businessfriel@gmail.com>
2021-08-09 02:02:43 -0400

    Range tooltip config.

--- src/components/Sorting/Controls/Controls.js
+++ src/components/Sorting/Controls/Controls.js
@@ -7,12 +7,21 @@ import PlayButton from "./PlayButton/PlayButton";
 
 const Controls = ({ handlePlay, handleReset }) => {
     const [range, setRange] = useState(50);
+    const [tooltip, setToolTip] = useState(false);
     const sortCtx = useContext(SortContext);
 
     const isDisabled = !sortCtx.sortSelection || !sortCtx.dataSelection;
 
     // const rangeNum = 50;
-    const tooltipPosition = `calc(${range}px - 10px)`;
+    // const tooltipPosition = `calc(${range}% - -4px)`;
+    const tooltipPosition = `${range}%`;
+
+    const handleMouseDown = () => {
+        setToolTip(true);
+    };
+    const handleMouseUp = () => {
+        setToolTip(false);
+    };
 
     return (
         <div className={classes.controls__container}>
@@ -24,8 +33,10 @@ const Controls = ({ handlePlay, handleReset }) => {
                 />
                 <div className={classes.controls__range}>
                     <div className={classes.tooltip}>
-                        <div style={{ left: tooltipPosition }} className={classes.tooltip__num}>
-                            50
+                        <div style={{ left: tooltipPosition }} className={classes.tooltip__thumb}>
+                            {tooltip && (
+                                <span className={classes.tooltip__thumb__num}>{range}</span>
+                            )}
                         </div>
                     </div>
                     <input
@@ -33,9 +44,12 @@ const Controls = ({ handlePlay, handleReset }) => {
                         type="range"
                         disabled={isDisabled}
                         min={1}
-                        max={100}
+                        max={101}
+                        step={1}
                         onChange={(e) => setRange(e.target.value)}
                         value={range}
+                        onMouseDown={handleMouseDown}
+                        onMouseUp={handleMouseUp}
                     />
                 </div>
             </div>
